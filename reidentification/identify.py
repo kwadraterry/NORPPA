619faa1323373d669387827b85aa210b42d5078a
Ekaterina Nepovinnykh <kwadraterry@gmail.com>
2023-10-06 22:26:23 +0300

    fix

--- reidentification/identify.py
+++ reidentification/identify.py
@@ -426,9 +426,12 @@ def get_label(db, i):
 def identify(query, database=None, topk=5, leave_one_out=False):
     if query is None:
         return [None]
+    query = [x for x in query if x is not None and x[0] is not None]
+    if len(query) == 0:
+        return [None]
+    
     if database is None:
         database = query
-    query = [x for x in query if x is not None and x[0] is not None]
     query_features = np.concatenate([f[np.newaxis,...] for (f, _) in query])
     
     def add_fisher_field(label, fisher):
